8ac9df7f3ef233ae22e7d7b10e39ad65049b21f2
soullesspy <maxgtmaster@hotmail.com>
2019-03-20 19:59:31 -0300

    Update todolist.js

--- js/todolist.js
+++ js/todolist.js
@@ -197,7 +197,7 @@
             //  - La llamada debe ser asíncrona.
             //  - No te olvides de envíar el parámetro para que se cree la tarea.
             if (inputText.getAttribute('value') != currentTask.description){
-                Ajax.sendPutRequest(API_URL,currentTask,MediaFormat.JSON,()=>revertHTMLChangeOnEdit(currentTask),() => showError(code, 'La tarea no ha podido ser editada.'),true);
+                Ajax.sendPutRequest(API_URL,currentTask,MediaFormat.JSON,()=>revertHTMLChangeOnEdit(currentTask),() => showError(),true);
             }
         };
 
